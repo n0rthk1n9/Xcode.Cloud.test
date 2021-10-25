d038f27da03e1228f0d52a44a5b863bd1300cc23
Jan Armbrust <jan.armbrust@googlemail.com>
2021-10-25 22:18:31 +0200

    Initial Commit

---
+++ Xcode.Cloud.test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode.Cloud.test
+//
+//  Created by Jan Armbrust on 25.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode.Cloud.test/Xcode_Cloud_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Cloud_testApp.swift
+//  Xcode.Cloud.test
+//
+//  Created by Jan Armbrust on 25.10.21.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Cloud_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
